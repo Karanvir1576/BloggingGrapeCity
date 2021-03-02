c6b230faa08c2dbc06d204c00ef3513f5184801a
KVSM <karanmalick@gmail.com>
2021-03-03 03:23:37 +0530

    Adding complete project

---
+++ Blogging/BloggingProject/Helper/UserLoginHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BloggingProject.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace BloggingProject.Helper
+{
+    public class UserLoginHelper
+    {
+        IConfiguration _configuration;
+
+        public UserLoginHelper(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public UserLogin ValidateUser(UserLogin login)
+        {
+            UserLogin user = null;
+            if (login.UserName == "GrapeCity")
+            {
+                user = new UserLogin { UserName = "GrapeCity", Password = "GrapeCityRocks123" }; //Kept in code for test purposes. Can be encrypted and stored in db.
+                return user;
+
+            }
+            return user;
+        }
+
+        public bool ValidatePassword(string password, UserLogin user)
+        {
+            if (password.CompareTo(user.Password) == 0)
+                return true;
+            return false;
+        }
+
+        public string GetAuthToken(UserLogin userLogin)
+        {
+            var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:SigningKey"]));
+
+            var securityToken = new JwtSecurityToken(_configuration["Jwt:Issuer"], null, null, null,DateTime.Now.AddHours(2.0), new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256));
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            return tokenHandler.WriteToken(securityToken);
+        }
+    }
+}
