83933a830f7e9292aea2104bd98733a9d133fcea
KVSM <karanmalick@gmail.com>
2021-03-04 23:17:12 +0530

    Add files via upload

---
+++ Blogging/BloggingProject/Services/BlogDbOperations.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using BloggingProject.Models;
+
+namespace BloggingProject.Services
+{
+    public class BlogDbOperations : IDbOperations
+    {
+        private readonly Test20Context _context;
+        public  BlogDbOperations(Test20Context context)
+        {
+            _context = context;
+        }
+        public object GetData(int userId=0)
+        {
+            return _context.Blogs.Where(item => _context.UserXBlogs.Any(userBlog => userBlog.BlogId == item.BlogId && userBlog.UserId == userId)).ToList().OrderByDescending(item => item.DttmRcdAdded);
+        }
+        public void InsertData(object blogModel,int userId)
+        {
+            var blog = (Blog)blogModel;
+            _context.Database.ExecuteSqlInterpolated($"CreateBlog {blog.BlogHeader}, {blog.Content},{userId}");//Called StoredProc for Inserting
+        }
+
+        public void UpdateData(object dbModel, object updateModel)
+        {
+            var dbBlog = (Blog)dbModel;
+            var blogToUpdate = (Blog)updateModel;
+            dbBlog.BlogHeader = blogToUpdate.BlogHeader;
+            dbBlog.Content = blogToUpdate.Content;
+            dbBlog.DttmRcdUpdated = DateTime.Now.ToString("yyyyMMddhhmmss");
+            _context.Update(dbModel);
+            _context.SaveChanges();
+        }
+
+        public void DeleteData(object deleteModel)
+        {
+            var blogToDelete = (Blog)deleteModel;
+            _context.Blogs.Remove(blogToDelete);
+            _context.SaveChanges();
+        }
+
+
+    }
+}
---
+++ Blogging/BloggingProject/Services/IDbOperations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using BloggingProject.Models;
+
+namespace BloggingProject.Services
+{
+    public interface IDbOperations
+    {
+        public object GetData(int userId=0);
+        public void UpdateData(object dbModel,object updateModel);
+        public void DeleteData(object deleteModel);
+        public void InsertData(object model,int userId=0); 
+    }
+}
